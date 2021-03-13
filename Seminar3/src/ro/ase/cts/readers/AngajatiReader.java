4f956a008b5dbc087455c8bfb9d35583cf141417
RotaruEmilianTeodor1086 <emilian.teodor18@gmail.com>
2021-03-13 13:49:28 +0200

    Update final seminar 3

---
+++ Seminar3/src/ro/ase/cts/readers/AngajatiReader.java
@@ -0,0 +1,35 @@
+package ro.ase.cts.readers;
+
+import ro.ase.cts.clase.Angajat;
+import ro.ase.cts.clase.Aplicant;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AngajatiReader extends AplicantiReader {
+
+    public AngajatiReader(String filename) {
+        super(filename);
+    }
+
+    public List<Aplicant> readAplicanti() throws FileNotFoundException {
+        Scanner input2 = new Scanner(new File(super.filename));
+        input2.useDelimiter(",");
+        List<Aplicant> angajati = new ArrayList<Aplicant>();
+
+        while (input2.hasNext()) {
+            Angajat angajat = new Angajat();
+            super.readAplicant(input2, angajat);
+            int salariu = input2.nextInt();
+            String ocupatie = input2.next();
+            angajat.setSalariu(salariu);
+            angajat.setOcupatie(ocupatie);
+            angajati.add(angajat);
+        }
+        input2.close();
+        return angajati;
+    }
+}
