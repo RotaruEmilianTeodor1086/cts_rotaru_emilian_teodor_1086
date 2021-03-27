8dc2ef8374be663db0bb474ef3f1da7ded59a063
RotaruEmilianTeodor1086 <emilian.teodor18@gmail.com>
2021-03-27 13:11:53 +0200

    Seminar 5 A4 prototype + simpleFactory

---
+++ Seminar5/src/ro/ase/cts/factory/program/Main.java
@@ -0,0 +1,17 @@
+package ro.ase.cts.factory.program;
+
+import ro.ase.cts.factory.clase.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Factory factory = new Factory();
+
+        PersonalMedical medic1 = factory.create(TipPersonal.MEDIC, "Ionescu", 5000);
+        PersonalMedical asistent1 = factory.create(TipPersonal.ASISTENT, "Popescu", 2000);
+        PersonalMedical brancardier1 = factory.create(TipPersonal.BRANCARDIER, "Marinescu", 3000);
+
+        System.out.println(medic1);
+        System.out.println(asistent1);
+        System.out.println(brancardier1);
+    }
+}
