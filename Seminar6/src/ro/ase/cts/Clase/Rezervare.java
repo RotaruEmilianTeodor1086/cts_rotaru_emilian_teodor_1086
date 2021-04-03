e39f8dfde86e6d6164beb1f28ec67800d88d6b9d
RotaruEmilianTeodor1086 <emilian.teodor18@gmail.com>
2021-04-03 13:25:39 +0300

    Update final seminar 6 (factory + builder)

---
+++ Seminar6/src/ro/ase/cts/Clase/Rezervare.java
@@ -0,0 +1,56 @@
+package ro.ase.cts.Clase;
+
+public class Rezervare {
+    private int codRezervare;
+    private boolean aremancareInclusa;
+    private boolean areScaunErgonomic;
+    private boolean areBauturaRacoritoareInclusa;
+    private boolean areMuzicaAmbientalaPersonalizata;
+    private String genMuzical;
+
+    public Rezervare(int codRezervare, boolean aremancareInclusa, boolean areScaunErgonomic, boolean areBauturaRacoritoareInclusa, boolean areMuzicaAmbientalaPersonalizata, String genMuzical) {
+        this.codRezervare = codRezervare;
+        this.aremancareInclusa = aremancareInclusa;
+        this.areScaunErgonomic = areScaunErgonomic;
+        this.areBauturaRacoritoareInclusa = areBauturaRacoritoareInclusa;
+        this.areMuzicaAmbientalaPersonalizata = areMuzicaAmbientalaPersonalizata;
+        this.genMuzical = genMuzical;
+    }
+
+    public void setCodRezervare(int codRezervare) {
+        this.codRezervare = codRezervare;
+    }
+
+    public void setAremancareInclusa(boolean aremancareInclusa) {
+        this.aremancareInclusa = aremancareInclusa;
+    }
+
+    public void setAreScaunErgonomic(boolean areScaunErgonomic) {
+        this.areScaunErgonomic = areScaunErgonomic;
+    }
+
+    public void setAreBauturaRacoritoareInclusa(boolean areBauturaRacoritoareInclusa) {
+        this.areBauturaRacoritoareInclusa = areBauturaRacoritoareInclusa;
+    }
+
+    public void setAreMuzicaAmbientalaPersonalizata(boolean areMuzicaAmbientalaPersonalizata) {
+        this.areMuzicaAmbientalaPersonalizata = areMuzicaAmbientalaPersonalizata;
+    }
+
+    public void setGenMuzical(String genMuzical) {
+        this.genMuzical = genMuzical;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Rezervare{");
+        sb.append("codRezervare=").append(codRezervare);
+        sb.append(", aremancareInclusa=").append(aremancareInclusa);
+        sb.append(", areScaunErgonomic=").append(areScaunErgonomic);
+        sb.append(", areBauturaRacoritoareInclusa=").append(areBauturaRacoritoareInclusa);
+        sb.append(", areMuzicaAmbientalaPersonalizata=").append(areMuzicaAmbientalaPersonalizata);
+        sb.append(", genMuzical='").append(genMuzical).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
--- Seminar6/src/ro/ase/cts/Program/Main.java
+++ Seminar6/src/ro/ase/cts/Program/Main.java
@@ -13,5 +13,28 @@ public class Main {
         afiseazaInformatiiJucator(new FabricaAtacant(), "Popescu", 15);
         afiseazaInformatiiJucator(new FabricaPortar(), "Ionescu", 17);
         afiseazaInformatiiJucator(new FabricaMijlocasi(), "Georgescu", 11);
+
+        System.out.println("---------------------");
+
+        Rezervare rezervare = new Rezervare(12, false,true, false, true, "pop");
+        BuilderRezervare builderRezervare = new BuilderRezervare().setAremancareInclusa(true).setAreMuzicaAmbientalaPersonalizata(true).setGenMuzical("Pop");
+        Rezervare rezervare1 = builderRezervare.build();
+
+        Rezervare rezervare2 = new BuilderRezervare().setCodRezervare(2).setAreScaunErgonomic(true).build();
+        System.out.println(rezervare1.toString());
+        System.out.println(rezervare2.toString());
+
+        Rezervare rezervare3 = new BuilderRezervare().setCodRezervare(3).setGenMuzical("Rock").setAreScaunErgonomic(true).build();
+        System.out.println(rezervare3.toString());
+
+        Rezervare rezervare4 = builderRezervare.setCodRezervare(4).build();
+
+        BuilderRezervare2 builderRezervare2 = new BuilderRezervare2().setAremancareInclusa(true).setAreScaunErgonomic(true).setAreBauturaRacoritoareInclusa(true);
+        Rezervare rezervare5 = builderRezervare2.setCodRezervare(10).build();
+        Rezervare rezervare6 = builderRezervare2.setCodRezervare(11).build();
+        System.out.println(rezervare5.toString());
+        System.out.println(rezervare6.toString());
     }
+
+
 }
